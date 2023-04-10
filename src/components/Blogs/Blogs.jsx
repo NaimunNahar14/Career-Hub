35cd1173bc8a9fd685248ea0424e0ec71302eaec
NaimunNahar14 <naimunnaharshoshi@gmail.com>
2023-04-10 13:07:49 -0700

    added chart

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -3,7 +3,35 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h2>this blog</h2>
+            <h2>Question and Answer:</h2>
+            <div>
+                <h3>1. when should you use context api?</h3>
+                <ul >
+                   <li> Ans: 
+                   The Issue With Using React Context API for State
+                    Even if only b changes ComponentA will re-render for no reason, it will render the same content. This is called an extra or unnecessary re-render. For that reason, it is bad practice to have a single, huge state provided by a context.
+                   </li>
+                </ul>
+                
+            </div>
+            <div>
+                <h3>2.what is a customer hook?</h3>
+                <p>Ans: The Hook Model is a four-phase process that businesses can use to create products or services used habitually by customers. The goal is to result in voluntary, high-frequency engagement. At its core, the Hook Model is about creating a customer habit.</p>         
+            </div>
+            <div>
+                <h3>3.What is useRef?</h3>
+                <ul>Ans:
+                useRef(initialValue) is a built-in React hook that accepts one argument as the initial value and returns a reference (aka ref). A reference is an object having a special property current .
+                    
+                </ul>               
+                
+            </div>
+            <div>
+                <h3>4. what is useMemo</h3>
+                <p >Ans: useMemo is a React Hook that lets you cache the result of a calculation between re-renders. const cachedValue = useMemo(calculateValue, dependencies) Reference. useMemo(calculateValue, dependencies)</p>
+
+
+            </div>
         </div>
     );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,68 @@
 import React from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 
 const Statistics = () => {
+    const data = [
+        {
+          name: 'Assignment-1',
+          Mark:55,
+          AvgMark: 60,
+        },
+        {
+            name: 'Assignment-2',
+          Mark:56,
+          AvgMark: 60,
+          
+        },
+        {
+            name: 'Assignment-3',
+            Mark:60,
+            AvgMark: 60,
+        },
+        {
+            name: 'Assignment-4',
+            Mark:43,
+            AvgMark: 60,
+        },
+        {
+            name: 'Assignment-5',
+            Mark:53,
+            AvgMark: 60,
+        },
+        {
+            name: 'Assignment-6',
+            Mark:48,
+            AvgMark: 60,
+        },
+        {
+            name: 'Assignment-7',
+            Mark:60,
+            AvgMark: 60,
+        },
+      ];
     return (
         <div>
-            <h2>This is a statistics page</h2>
+        {/* <ResponsiveContainer width="100%" height="100%">    */}
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          
+        >
+          {/* <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend /> */}
+          <Bar dataKey="Mark" fill="#8884d8"></Bar>
+          <Bar dataKey="AgvMark" fill="#8884d8" ></Bar>
+          <XAxis dataKey="name"></XAxis>
+          <YAxis></YAxis>
+          <Tooltip></Tooltip>
+          <Legend></Legend>
+        </BarChart>
+      {/* </ResponsiveContainer> */}
         </div>
     );
 };
