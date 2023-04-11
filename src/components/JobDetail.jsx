79c59c948774c207c5f27b86a12540178f4bca1f
NaimunNahar14 <naimunnaharshoshi@gmail.com>
2023-04-11 11:02:01 -0700

    complete jobdetails

--- src/components/JobDetail.jsx
+++ src/components/JobDetail.jsx
@@ -14,10 +14,37 @@ const JobDetail = () => {
     },[])
     return (
          <div>
-            <h2>{Data.company_name}</h2>
-            <h2>{Data.educational_requirements}</h2>
-            <h4>{Data.job_responsibility}</h4>
-        </div>
+                
+                <div>
+                <h1 className='text-center mt-9'>Job Details</h1>
+                <div>
+                <div>
+                    <div className='flex flex-col content-center flex-wrap items-end mt-6'>
+                    <div>
+                        <h2><span className='font-bold'>Job-Description</span>: {Data.job_description}</h2>
+                        <h2><span className='font-bold'>Responsibility</span>: {Data.job_responsibility}</h2>
+                        <h2><span className='font-bold'>Education Requirements</span>: {Data.educational_requirements}</h2>
+                        <h2><span className='font-bold'>Experience</span>: {Data.experiences}</h2>
+                    </div>
+                    <div className='bg-slate-200 rounded-lg w-80'>
+                        <h2 className='font-bold'>Job Details</h2>
+                        <hr />   
+                        <h3><span>Salary</span>: {Data.salary}</h3>
+                        <h3><span>Job Title</span>: {Data.job_title}</h3>
+                        <h2 className='font-bold'>Contact Information</h2>
+                        <hr />
+                        <h3><span>Phone</span>: +88013457889</h3>
+                        <h3><span>E-mail</span>: jobs@acmecorp.com</h3>
+                        <h3><span>Location</span>: {Data.location}</h3>
+                    <button className='mb-3 bg-blue-400'>Apply Now</button>
+                    
+                    </div>
+                </div>
+                    </div>
+                </div>
+                </div>
+                </div>
+    
     );
 };
 
